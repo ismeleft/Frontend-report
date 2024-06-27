a12cdb6760de096bea047d743cfdad145c5f27c6
陳玟穎 <jessicachen@yile.com.tw>
2024-06-27 14:31:10 +0800

    feat: integrate React Router for route management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Experience from "./components/Experience/Experience";
+import Habit from "./components/Habit/Habit";
+import Root from "./routes/root";
+import ErrorPage from "./errorPage";
+import Education from "./components/Education/Education";
+import Target from "./components/Target/Target";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/education",
+    element: <Education />,
+  },
+  {
+    path: "/experience",
+    element: <Experience />,
+  },
+  {
+    path: "/habit",
+    element: <Habit />,
+  },
+  {
+    path: "/target",
+    element: <Target />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
