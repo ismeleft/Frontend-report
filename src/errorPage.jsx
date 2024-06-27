53ec412cc43e941786142a99fcf90c72ea4b93c1
陳玟穎 <jessicachen@yile.com.tw>
2024-06-27 11:31:21 +0800

    style: add style for error page

--- src/errorPage.jsx
+++ src/errorPage.jsx
@@ -1,15 +1,16 @@
+import { Stack } from "@mui/material";
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error)
     return (
-        <div id="error-page">
+        <Stack direction="column" justifyContent="center" alignItems="center" height="100vh">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
                 <i>{error.statusText || error.message}</i>
             </p>
-        </div>
+        </Stack>
     );
 }
