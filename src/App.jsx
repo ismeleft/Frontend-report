07eb6c02d71bd8da15698be22d19111640e819c6
ying <leftleft0813@gmail.com>
2024-07-25 14:09:25 +0800

    feat: add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-// import FallingText from "./components/FallingText/FallingText";
+import "./App.css";
 
-export const App = () => {
-  return <>{/* <FallingText/> */}</>;
+const App = () => {
+  const keywords = [
+    "# Web 前端",
+    "# INFJ",
+    "# 排球",
+    "# 桌球",
+    "# 狗派",
+    "# 獨立音樂",
+    "# KPOP",
+    "# 現場表演",
+    "# 電影",
+  ];
+
+  return (
+    <div className="landingPage">
+      <div className="pacman-container">
+        <svg className="pacman">
+          <circle cx="50" cy="50" r="25" />
+        </svg>
+        <svg className="eye">
+          <circle cx="6" cy="6" r="6" />
+        </svg>
+        <div className="keywords-container">
+          {keywords.map((keyword, index) => (
+            <div
+              className="keyword"
+              key={keyword}
+              style={{ animationDelay: `${index * 5.5}s` }}
+            >
+              {keyword}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
