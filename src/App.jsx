52cc587cced47ca99707eaa5c60e66c03eaa40eb
ying <leftleft0813@gmail.com>
2024-09-08 14:41:09 +0800

    style: 調整主視覺

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,26 @@
 import "./App.css";
+import { Stack, Typography } from "@mui/material";
 
 const App = () => {
-  const keywords = [
-    "# Web 前端",
-    "# INFJ",
-    "# 排球",
-    "# 桌球",
-    "# 狗派",
-    "# 獨立音樂",
-    "# KPOP",
-    "# 現場表演",
-    "# 電影",
-  ];
-
   return (
-    <div className="landingPage">
-      <div className="pacman-container">
-        <svg className="pacman">
-          <circle cx="50" cy="50" r="25" />
-        </svg>
-        <svg className="eye">
-          <circle cx="6" cy="6" r="6" />
-        </svg>
-        <div className="keywords-container">
-          {keywords.map((keyword, index) => (
-            <div
-              className="keyword"
-              key={keyword}
-              style={{ animationDelay: `${index * 5.5}s` }}
-            >
-              {keyword}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    <Stack sx={{ position: "relative" }}>
+      <Typography
+        variant="h1"
+        sx={{
+          position: "absolute",
+          top: "20%",
+          width: "100%",
+          textAlign: "center",
+          fontFamily: '"Noto Serif Khitan Small Script", serif',
+          fontWeight: 400,
+          fontStyle: "normal",
+          color: "#F28500",
+        }}
+      >
+        Hello World
+      </Typography>
+      <img className="background" src="/image/ocean.jpeg" alt="landing page" />
+    </Stack>
   );
 };
 
